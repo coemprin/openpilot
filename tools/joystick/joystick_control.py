633b41efc649749a6b29fadc7f2bae60483beaf9
Corentin EMP <corentin.emprin@michelin.com>
2025-07-23 09:48:57 +0200

    modification of joystick_control

--- tools/joystick/joystick_control.py
+++ tools/joystick/joystick_control.py
@@ -4,6 +4,8 @@ import argparse
 import threading
 import numpy as np
 from inputs import UnpluggedError, get_gamepad
+import sys
+import time
 
 from cereal import messaging
 from openpilot.common.params import Params
@@ -13,6 +15,8 @@ from openpilot.tools.lib.kbhit import KBHit
 
 EXPO = 0.4
 
+LX, LY, RX, RY, LT, RT = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+accelReceiver, steerReceiver = False, False
 
 class Keyboard:
   def __init__(self):
@@ -40,6 +44,56 @@ class Keyboard:
     return True
 
 
+# class Joystick:
+#   def __init__(self):
+#     # This class supports a PlayStation 5 DualSense controller on the comma 3X
+#     # TODO: find a way to get this from API or detect gamepad/PC, perhaps "inputs" doesn't support it
+#     self.cancel_button = 'BTN_NORTH'  # BTN_NORTH=X/triangle
+#     if HARDWARE.get_device_type() == 'pc':
+#       accel_axis = 'ABS_Z'
+#       steer_axis = 'ABS_RX'
+#       # TODO: once the longcontrol API is finalized, we can replace this with outputting gas/brake and steering
+#       self.flip_map = {'ABS_RZ': accel_axis}
+#     else:
+#       accel_axis = 'ABS_RX'
+#       steer_axis = 'ABS_Z'
+#       self.flip_map = {'ABS_RY': accel_axis}
+
+#     self.min_axis_value = {accel_axis: 0., steer_axis: 0.}
+#     self.max_axis_value = {accel_axis: 255., steer_axis: 255.}
+#     self.axes_values = {accel_axis: 0., steer_axis: 0.}
+#     self.axes_order = [accel_axis, steer_axis]
+#     self.cancel = False
+
+#   def update(self):
+#     try:
+#       joystick_event = get_gamepad()[0]
+#     except (OSError, UnpluggedError):
+#       self.axes_values = {ax: 0. for ax in self.axes_values}
+#       return False
+
+#     event = (joystick_event.code, joystick_event.state)
+
+#     # flip left trigger to negative accel
+#     if event[0] in self.flip_map:
+#       event = (self.flip_map[event[0]], -event[1])
+
+#     if event[0] == self.cancel_button:
+#       if event[1] == 1:
+#         self.cancel = True
+#       elif event[1] == 0:   # state 0 is falling edge
+#         self.cancel = False
+#     elif event[0] in self.axes_values:
+#       self.max_axis_value[event[0]] = max(event[1], self.max_axis_value[event[0]])
+#       self.min_axis_value[event[0]] = min(event[1], self.min_axis_value[event[0]])
+
+#       norm = -float(np.interp(event[1], [self.min_axis_value[event[0]], self.max_axis_value[event[0]]], [-1., 1.]))
+#       norm = norm if abs(norm) > 0.03 else 0.  # center can be noisy, deadzone of 3%
+#       self.axes_values[event[0]] = EXPO * norm ** 3 + (1 - EXPO) * norm  # less action near center for fine control
+#     else:
+#       return False
+#     return True
+
 class Joystick:
   def __init__(self):
     # This class supports a PlayStation 5 DualSense controller on the comma 3X
@@ -55,64 +109,38 @@ class Joystick:
       steer_axis = 'ABS_Z'
       self.flip_map = {'ABS_RY': accel_axis}
 
-    self.min_axis_value = {accel_axis: 0., steer_axis: 0.}
-    self.max_axis_value = {accel_axis: 255., steer_axis: 255.}
+    #self.min_axis_value = {accel_axis: 0., steer_axis: 0.}
+    #self.max_axis_value = {accel_axis: 255., steer_axis: 255.}
     self.axes_values = {accel_axis: 0., steer_axis: 0.}
     self.axes_order = [accel_axis, steer_axis]
     self.cancel = False
 
   def update(self):
+
+    global LX, LY, RX, RY, LT, RT
+    data = sys.stdin.readline().strip()
+    if data == "STOP":
+        print("Commande d'arrêt reçue, arrêt du receiver.")
+
     try:
-      joystick_event = get_gamepad()[0]
-    except (OSError, UnpluggedError):
-      self.axes_values = {ax: 0. for ax in self.axes_values}
-      return False
+        values = list(map(float, data.split()))
+        if len(values) == 10:
+            LX, LY, RX, RY, LT, RT, A, B, X, Y= values
 
-    event = (joystick_event.code, joystick_event.state)
+        else:
+            print(f"Erreur : Données incorrectes reçues -> {data}")
 
-    # flip left trigger to negative accel
-    if event[0] in self.flip_map:
-      event = (self.flip_map[event[0]], -event[1])
+    except (ValueError, KeyError) as e:
+        print(f"Erreur de parsing : {e}")
 
-    if event[0] == self.cancel_button:
-      if event[1] == 1:
-        self.cancel = True
-      elif event[1] == 0:   # state 0 is falling edge
-        self.cancel = False
-    elif event[0] in self.axes_values:
-      self.max_axis_value[event[0]] = max(event[1], self.max_axis_value[event[0]])
-      self.min_axis_value[event[0]] = min(event[1], self.min_axis_value[event[0]])
+    normAcc = float((RT + 1) - (LT + 1))
+    normAcc = normAcc if abs(normAcc) > 0.03 else 0.  # center can be noisy, deadzone of 3%
+    self.axes_values[0] = EXPO * normAcc ** 3 + (1 - EXPO) * normAcc  # less action near center for fine control
 
-      norm = -float(np.interp(event[1], [self.min_axis_value[event[0]], self.max_axis_value[event[0]]], [-1., 1.]))
-      norm = norm if abs(norm) > 0.03 else 0.  # center can be noisy, deadzone of 3%
-      self.axes_values[event[0]] = EXPO * norm ** 3 + (1 - EXPO) * norm  # less action near center for fine control
-    else:
-      return False
-    return True
+    normStee = float(LX)
+    normStee = normStee if abs(normStee) > 0.03 else 0.  # center can be noisy, deadzone of 3%
+    self.axes_values[1] = EXPO * normStee ** 3 + (1 - EXPO) * normStee  # less action near center for fine control
 
-class J2:
-  def __init__(self):
-    self.kb = KBHit()
-    self.axis_increment = 0.05  # 5% of full actuation each key press
-    self.axes_map = {'w': 'gb', 's': 'gb',
-                     'a': 'steer', 'd': 'steer'}
-    self.axes_values = {'gb': 0., 'steer': 0.}
-    self.axes_order = ['gb', 'steer']
-    self.cancel = False
-
-  def update(self):
-    key = self.kb.getch().lower()
-    self.cancel = False
-    if key == 'r':
-      self.axes_values = {ax: 0. for ax in self.axes_values}
-    elif key == 'c':
-      self.cancel = True
-    elif key in self.axes_map:
-      axis = self.axes_map[key]
-      incr = self.axis_increment if key in ['w', 'a'] else -self.axis_increment
-      self.axes_values[axis] = float(np.clip(self.axes_values[axis] + incr, -1, 1))
-    else:
-      return False
     return True
 
 def send_thread(joystick):
